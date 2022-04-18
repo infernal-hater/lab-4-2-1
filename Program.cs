f68e13707c7c7ca518e0a531fba0c62c253f5a22
Ivan Fakoff <ifakov239@gmail.com>
2022-04-18 18:28:54 +0300

    Первый пункт программы выполнен. Осталась одна задача, предыдущей ... лучше.

--- Program.cs
+++ Program.cs
@@ -8,9 +8,10 @@ namespace lab_4_2_1
         {
             Random rnd = new Random(); // Необходим лишь раз чисто для рандома.
             int arr_rows = 0, arr_columns = 0; //arr_rows - Кол-во строк массива, arr_columns - кол-во столбцов массива.
-                                               // Что интересно. Далее по тексту я буду создавать массив. И пока я при инициализации не присвою значения данным переменным,
-                                               // массив я не смогу объявить. Даже с учётом того, что я задал ввод с клавиатуры с присвоением значений.
+              // Что интересно. Далее по тексту я буду создавать массив. И пока я при инициализации не присвою значения данным переменным,
+              // массив я не смогу объявить. Даже с учётом того, что я задал ввод с клавиатуры с присвоением значений.
             byte choice = 0; // Пригодится для switch-case
+            int row_count = 0, zero_count = 0; // Счетчик нулевых элементов и строк без нулевых элементов.
 
             // Запрос у пользователя размера матрицы. Вначале строки, потом столбцы.
             Console.Write("Enter the number of rows in the array: ");
@@ -27,9 +28,8 @@ namespace lab_4_2_1
                 "1) Randomise \n" +
                 "2) By myself \n");
 
-            do
+            while (!(choice == 1 || choice == 2))
             {
-
                 Console.Write("Choose your method: ");
                 choice = Convert.ToByte(Console.ReadLine()); // Тут конвертация не в Int32! 
                 Console.WriteLine();
@@ -37,21 +37,27 @@ namespace lab_4_2_1
                 switch (choice)
                 {
                     case 1: // Заполнение через рандом.
-                        Console.WriteLine("That's your array:");
                         for (int i = 0; i < arr_rows; i++)
                         {
                             for (int j = 0; j < arr_columns; j++)
                             {
-                                array[i, j] = rnd.Next(-256, 256); // Рандомизация значений.
-                                Console.Write($"{array[i, j], -6}"); // Красивый вывод.
+                                array[i, j] = rnd.Next(-5, 5); // Рандомизация значений.                            
+                            }
+                        }
+                            break;
+
+                    case 2: // Заполнение через клавиатуру.
+                        Console.WriteLine("Enter a value and press Enter.");
+                        for (int i = 0; i < arr_rows; i++)
+                        {
+                            for (int j = 0; j < arr_columns; j++)
+                            {
+                                Console.Write($"[{i}, {j}] = "); // Рандомизация значений.
+                                array[i, j] = Convert.ToInt32(Console.ReadLine()); // Красивый вывод.
                             }
                             Console.WriteLine();
                         }
                         Console.WriteLine();
-                        break;
-
-                    case 2: // Заполнение через клавиатуру.
-                        Console.WriteLine("Test2");
                         break;
 
                     default: // Если пользователь де... неверно ввел ответ.
@@ -59,7 +65,24 @@ namespace lab_4_2_1
                         break;
                 }
 
-            } while (choice != 1 || choice != 2);
+            }
+
+            Console.WriteLine("That's your array:"); // Красивый вывод.
+            for (int i = 0; i < arr_rows; i++)
+            {
+                for (int j = 0; j < arr_columns; j++)
+                {
+                    Console.Write($"{array[i, j],-6}");
+                    if (array[i, j] == 0) // Заодно подсчитаем количество строк без нулей.
+                        zero_count++; // Вначале считаем сколько в строке нулей.
+                }
+                if (zero_count == 0) // В конце прогона строки смотрим количество нулей в строке и
+                    row_count++; // если их количество равно нулю, то повышаем счётчик безнулевых строк на один.
+                zero_count = 0; // И не забываем обнулить счётчик нулевых элементов в строке. 20 минут искал проблему, б...
+                Console.WriteLine();
+            }
+            Console.WriteLine($"\nArray has {row_count} rows without zero."); // И получаем красивый ответ по поводу безнулевых строк.
+
 
         }
     }
