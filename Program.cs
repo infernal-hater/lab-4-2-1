b2db291ff542cc18b0bd8a9fe6e029dba0719d4e
Ivan Fakoff <ifakov239@gmail.com>
2022-04-18 15:24:45 +0300

    Кто эти комментарии вообще читает?

--- Program.cs
+++ Program.cs
@@ -4,10 +4,17 @@ namespace lab_4_2_1
 {
     class Program
     {
-        static void Main()
+        public static void Main()
         {
+            int arr_rows = 0, arr_columns; //arr_rows - Кол-во строк массива, arr_columns - кол-во столбцов массива.
+            Console.Write("Enter the number of rows in the array:");
+            arr_rows = Convert.ToInt32(Console.Read()); // Присвоение целочисленного значения через чтение с клавиатуры.
+            // Очень неудобно после С++. Значение = Конвертировать.ВЦелочисл(Чтение с клавиатруы());
 
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter the number of columns in the array:");
+            arr_rows = Convert.ToInt32(Console.Read());
         }
     }
+
+
 }
